d03e4d118ade44bcd58bee535d48c1f478c98af3
Mohammed b. Terry b. Jack <32687826+mohammedterry@users.noreply.github.com>
2017-12-23 00:22:17 +0000

    Update rnn_sdr.py
    update so that train cuts out after reaching errThresh or iterations

--- rnn_sdr.py
+++ rnn_sdr.py
@@ -41,16 +41,17 @@ class RNN:
         return self.sigmoid( self.l2.f(hidden_value) )
 
     def forward(self, i, o):
+        prediction = np.zeros_like(i)
         for n in range(self.seq_length): #going through binary 
-            input_bits = np.array([[i[n]]])
-            output_bits = np.array([[o[n]]]).T
+            input_bits = np.array([[i[self.seq_length - n - 1]]])
+            output_bits = np.array([[o[self.seq_length - n - 1]]]).T
             p = self.predict(input_bits, remember = True)
             e = output_bits - p  #error
             self.deltas.append( e * self.d_sigmoid( p ) )
             self.error += np.abs(e[0])
-        return p
+            prediction[self.seq_length - n - 1] = int(np.round(p))
+        return prediction
     
-
     def backward(self,i):
         hidden_delta_future = np.zeros(self.d_h)
         for n in range(self.seq_length): 
@@ -61,7 +62,7 @@ class RNN:
 
             hidden_delta = (hidden_delta_future.dot( self.l1.weights.T) +  output_delta.dot( self.l2.weights.T )) * self.d_sigmoid( hidden_value)
 
-            self.l2.updates += np.atleast_2d(hidden_value.T.dot(output_delta))
+            self.l2.updates += np.atleast_2d(hidden_value).T.dot(output_delta)
             self.l1.updates += np.atleast_2d(prev_hidden_value).T.dot(hidden_delta)
             self.l0.updates += input_bits.T.dot(hidden_delta)        
             
@@ -76,9 +77,13 @@ class RNN:
         self.deltas = []
         self.memory = []
         self.memory.append(np.zeros(self.d_h)) #memory's initial time step is zero since no sequence has came before it
+        
+        self.l2.updates *= 0
+        self.l1.updates *= 0
+        self.l0.updates *= 0
 
-    def train(self, training_inputs, training_outputs, alpha = 0.1, iterations = 10000, log_rate = 1000):
-        for i in range(iterations):
+    def train(self, training_inputs, training_outputs, errorThresh = 0.01, alpha = 0.1, iterations = 10000, log_rate = 1000):
+        for i in range(iterations): #or self.error < errorThresh
             
             self.init_memory()
             p = self.forward(training_inputs[i],training_outputs[i])
@@ -89,12 +94,9 @@ class RNN:
             self.l2.adjust(alpha)
 
             if(i % log_rate == 0):
-                prediction = ''
-                for bit in range(self.seq_length): # [[0.3312]]
-                    prediction += str(int(np.round(p[0][0]))) + " "
                 print("Input:", str(training_inputs[i]))
                 print("Expected:", str(training_outputs[i]))
-                print("Predicted: [" + prediction +"]")
+                print("Predicted:", str(p))
                 print("Error:", str(self.error))
                 print("------------")
         self.dump_to_disk()
@@ -106,13 +108,11 @@ for i in range(2**8):
     int2binary[i] = binary[i] #[{...2:[0 0 0 0 0 0 1 0], ...}]
 
 A, B = [], []
-for i in range(10): #make a quick training set
+for i in range(100000): #make a quick training set
     a_int = np.random.randint((2**8)/4) # int version
     A.append(int2binary[a_int]) # binary encoding
     b_int = a_int *2
     B.append(int2binary[b_int]) # binary encoding
-A *= 500
-B *= 500
 #-----TEST-------
 net = RNN([1,3,1], 8)
-net.train(A,B, iterations = 500, log_rate = 5)
+net.train(A,B)
